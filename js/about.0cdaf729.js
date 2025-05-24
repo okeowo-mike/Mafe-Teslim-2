844e4276d8c0d049d6b8f2b51fa3af377076eb59
okeowo-mike <okeowom7@gmail.com>
2025-05-24 12:54:32 +0100

    Updates

---
+++ js/about.0cdaf729.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmafe2"]=self["webpackChunkmafe2"]||[]).push([[594],{603:function(n,u,a){a.r(u),a.d(u,{default:function(){return i}});var e=a(6768);const t={class:"about"};function r(n,u){return(0,e.uX)(),(0,e.CE)("div",t,u[0]||(u[0]=[(0,e.Lk)("h1",null,"This is an about page",-1)]))}var s=a(1241);const c={},f=(0,s.A)(c,[["render",r]]);var i=f}}]);
+//# sourceMappingURL=about.0cdaf729.js.map
